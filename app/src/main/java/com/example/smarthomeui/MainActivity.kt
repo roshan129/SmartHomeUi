fc2b9b43962d45e162f6e43bbf92e280e2f6415e
roshan129 <roshan@iprogrammer.co>
2023-01-14 12:23:51 +0530

    snackbar and scaffold

--- app/src/main/java/com/example/smarthomeui/MainActivity.kt
+++ app/src/main/java/com/example/smarthomeui/MainActivity.kt
@@ -5,20 +5,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.smarthomeui.ui.theme.SmartHomeUiTheme
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -27,25 +42,50 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background,
                 ) {
 
-                    val boxColor  = remember {
-                        mutableStateOf(Color.Green)
+                    val textString = remember {
+                        mutableStateOf("")
                     }
-                    Column {
-                        MainColorBox(
+                    val snackBarHostState  = SnackbarHostState()
+                    val scope  = rememberCoroutineScope()
+
+                    Scaffold(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        snackbarHost = {
+                            SnackbarHost(hostState = snackBarHostState)
+                        },
+                    ) {
+                        Column(
                             modifier = Modifier
-                                .fillMaxSize()
-                                .weight(0.5f)
-                                .background(Color.Red)
-                            ) {
-                            boxColor.value = it
+                                .padding(it)
+                                .fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+
+                            TextField(
+                                value = textString.value,
+                                onValueChange = { it: String ->
+                                    textString.value = it
+                                },
+                                label = {
+                                    Text(text = "Please Enter Name")
+                                }
+                            )
+                            Button(onClick = {
+                                scope.launch {
+                                    snackBarHostState.showSnackbar(
+                                        "Hello ${textString.value}"
+                                    )
+                                }
+
+                            }) {
+                                Text(text = "Greet Me")
+                            }
+
                         }
-                        ColorChangingBox(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .weight(0.5f)
-                                .background(boxColor.value)
-                        )
                     }
+
 
                 }
             }
@@ -54,31 +94,5 @@ class MainActivity : ComponentActivity() {
 
 }
 
-@Composable
-fun MainColorBox(
-    modifier: Modifier,
-    onMainBoxClicked: (Color) -> Unit
-) {
 
-    Box(
-        modifier = modifier.clickable {
-            onMainBoxClicked(
-                Color(
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                )
-            )
-        }
-    ) {
 
-    }
-}
-
-@Composable
-fun ColorChangingBox(modifier: Modifier) {
-
-    Box(
-        modifier = modifier
-    )
-}
